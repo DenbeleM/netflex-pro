ef65813dc5e49fb9ebe6c7a4dce9cc23a37c8664
DenbeleM <tibebu.kejela-ug@aau.edu.et>
2024-03-16 04:35:13 -0300

    my first comit

---
+++ vite-project/src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Heade from "../Components/Header/Heade";
+import Footer from "../Components/FOOTER/Footer";
+
+const Home = () => {
+  return (
+    <div>
+      <Heade />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
